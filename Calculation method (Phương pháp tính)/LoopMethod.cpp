ab2450a7368f2d4d80b29cf6ceda09399b750301
Giáo Sư Đầy Bụng <37433279+MinhCuong99er@users.noreply.github.com>
2018-10-15 20:52:29 +0700

    Xấp xỉ nghiệm của pt f(x) = 0 bằng phương pháp lặp

--- Calculation method (Phương pháp tính)/LoopMethod.cpp
+++ Calculation method (Phương pháp tính)/LoopMethod.cpp
@@ -1,17 +1,24 @@
-//The approximation of the equation
-// Xấp xỉ nghiệm của pt f(x) = 0
-
+/*LoopMethod.cpp
+  The approximation of the equation
+  Xấp xỉ nghiệm của pt f(x) = 0 */
+#include <stdlib.h>
 #include <cmath>
+#include <ctime>
 #include <iostream>
 using namespace std;
 
 double caculacateF_x (double x) // Khai báo hàm f(x)
 {
-    return x*cos(x) - 2*x*x + 3*x -1 ;
+    return  pow(1+4*x,0.25) ;
+}
+double random(double a, double b) // Hàm Random số thực từ a đến b
+{
+   return a + (b - a)*rand()/RAND_MAX;
 }
 void loopMethod (double a, double b, double c) // phương pháp lặp
 {
-    double x = min(a,b),y;
+    srand(time(NULL));
+    double x = random(a,b),y;
     int count=0;
     while (abs(x-y) > c)
     {
@@ -30,8 +37,7 @@ int main ()
     cout << "Nhap sai so / do chinh xac: ";
     cin >> accuracy;
 
-    cout << endl;
-    cout << "Ap dung phuong phap lap:\n";
+    cout << "\nAp dung phuong phap lap:\n";
     loopMethod(first,last,accuracy);
     return 0;
 }
