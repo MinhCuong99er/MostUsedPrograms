39b33721169f68e0ea349fd8d9c12c2b88006570
Giáo Sư Đầy Bụng <37433279+MinhCuong99er@users.noreply.github.com>
2018-10-28 12:47:16 +0700

    Update InterpolationFormula.cpp

--- Calculation method (Phương pháp tính)/InterpolationFormula.cpp
+++ Calculation method (Phương pháp tính)/InterpolationFormula.cpp
@@ -1,11 +1,10 @@
-// Áp dụng Công thức nội suy Lagrange và Newton tính gần đúng giá trị đa thức 
-
+// Áp dụng Công thức nội suy Lagrange và Newton tính gần đúng giá trị đa thức
 #include <cmath>
 #include <iostream>
 using namespace std;
 
 // Công thức nội suy Lagrange
-double LagrangeInterpolationFormula (double x[], double y[], int k, double c)
+double LagrangeInterpolationFormula (double x[], double y[], int k, double c) 
 {
     double p = 0,Lagrange[k];
     int i,j;
@@ -23,17 +22,17 @@ double LagrangeInterpolationFormula (double x[], double y[], int k, double c)
     return p;
 }
 
-// Công thức nội suy Newton
-/*
+/* Công thức nội suy Newton
+
     Hàm kiểm tra sai phân tiến hay lùi:
         + Tiến trả về 1.
         + Lùi trả về 0.
         + Còn lại trả về -1.
 */
-int invalidProcessOrBackward (float x[])
+int invalidProcessOrBackward (double x[])
 {
     long i,count=0;
-    long n = sizeof(x)/sizeof(float);
+    long n = sizeof(x)/sizeof(double);
     for (i=0;i<n-1;i++)
     {
         if (x[i]<x[i+1]) count++;
@@ -42,11 +41,11 @@ int invalidProcessOrBackward (float x[])
 }
 double caculateSignal (int i,int j, double x[], double y[]) // Tính tỷ hiệu
 {
-    if (j==i+1) return (y[j]-y[i])/(x[j]-x[i]);
+    if (j==i+1 ) return (y[j]-y[i])/(x[j]-x[i]);
     else
         return (caculateSignal(i+1,j,x,y)/(x[j]-x[i]) - caculateSignal(i,j-1,x,y)/(x[j]-x[i]));
 }
-double NewtonInterpolationFormula (double x[], double y[],int k, double c)
+double NewtonProgressInterpolationFormula (double x[], double y[],int k, double c) // Nôi suy Newton tiến
 {
     int i,j;
     double p = y[0];
@@ -55,6 +54,18 @@ double NewtonInterpolationFormula (double x[], double y[],int k, double c)
         double temp = 1.0;
         for (j=0;j<i;j++) temp *= (c-x[j]);
         p += temp*caculateSignal(0,i,x,y);
+    }
+    return p;
+}
+double NewtonBackInterpolationFormula (double x[], double y[],int k, double c) // Nôi suy Newton lùi
+{
+    int i,j;
+    double p = y[k-1];
+    for (i=k-2;i>=0;i--)
+    {
+        double temp = 1.0;
+        for (j=k-2;j>=1;j--) temp *= (c-x[j]);
+        p += temp*caculateSignal(i-k,i,x,y);
     }
     return p;
 }
@@ -82,6 +93,9 @@ int main ()
     cout << "\nGia tri gan dung cua da thuc tai x = c la: \n+ Ap dung cong thuc noi suy Lagrange: f(c) ~ "
          << LagrangeInterpolationFormula(x,y,n+1,c)
          << "\n+ Ap dung cong thuc noi suy Newton: f(c) ~ "
-         << NewtonInterpolationFormula(x,y,n+1,c);
+    //if (invalidProcessOrBackward(x)==1)
+         << NewtonProgressInterpolationFormula(x,y,n+1,c);
+    //else if (invalidProcessOrBackward(x)==0)
+        //cout << NewtonBackInterpolationFormula(x,y,n+1,c);
     return 0;
 }
